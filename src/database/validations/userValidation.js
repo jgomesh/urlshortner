0eafa7c9217313dc0e5c2abc3beec0da5ade3b2e
jhugill <112505045+jhugill@users.noreply.github.com>
2024-08-31 18:08:05 -0300

    feature: validate if user data is valid

---
+++ src/database/validations/userValidation.js
@@ -0,0 +1,21 @@
+const Joi = require('joi');
+
+const userValidation = Joi.object({
+  name: Joi.string().trim().min(3).max(25).required(),
+  email: Joi.string().email().trim().required(),
+  password: Joi.string().min(6).required(),
+});
+
+const validateUser = (req, res, next) => {
+  const user = req.body;
+  const { error } = userValidation.validate(user, { abortEarly: false });
+
+  if (error) {
+    const errorMessages = error.details.map(err => err.message);
+    return res.status(400).json({ errors: errorMessages });
+  }
+
+  next();
+};
+
+module.exports = validateUser;
