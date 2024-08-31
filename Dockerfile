eaad70d36df312a1817f4eb2a54d9ffadca524ef
jhugill <112505045+jhugill@users.noreply.github.com>
2024-08-31 18:14:11 -0300

    feature: using docker-compose enviroment and log information with new relic

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:20.7
+
+EXPOSE 3001
+
+WORKDIR /backend
+
+ENV NEW_RELIC_NO_CONFIG_FILE=true
+ENV NEW_RELIC_DISTRIBUTED_TRACING_ENABLED=true
+ENV NEW_RELIC_LOG=stdout
+ENV NEW_RELIC_APP_NAME="URL Shortner"
+
+COPY package.json package-lock*.json .sequelizerc ./
+
+RUN npm install && npm rebuild bcrypt --build-from-source
+
+COPY . .a
+
+CMD ["npm", "start"]
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const users = require('./database/routes/users');
+const shortenedUrl = require('./database/routes/shortedUrls');
 const sequelize = require('./database/instances/sequelize');
 const dotenv = require('dotenv');
+require('newrelic');
 
 dotenv.config();
 
@@ -12,6 +14,7 @@ const port = process.env.PORT || 3001;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(shortenedUrl);
 app.use(users);
 
 sequelize.sync().then(() => console.log('Banco de dados conectado:'));
