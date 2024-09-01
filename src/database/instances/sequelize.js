38dae328297f3f08543c61e7d7266ee9cabc3fc4
jhugill <112505045+jhugill@users.noreply.github.com>
2024-09-01 11:48:28 -0300

    fix

--- src/database/instances/sequelize.js
+++ src/database/instances/sequelize.js
@@ -4,10 +4,9 @@ require('dotenv/config');
 
 const connectionString = `postgresql://postgres.zjmkwwrisiqabdajxbdu:${process.env.DB_PASS}@aws-0-us-west-1.pooler.supabase.com:6543/postgres`;
 
-// Cria uma instância do Sequelize com a string de conexão
 const sequelize = new Sequelize(connectionString, {
   dialect: 'postgres',
-  logging: false, // Desativa o logging de consultas SQL (opcional)
+  logging: false,
 });
 
 sequelize.authenticate();
